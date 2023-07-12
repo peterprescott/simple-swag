0928a9378468e5ef19edaffd3e400b8ab70a9e84
Peter Prescott <prescott.peter@gmail.com>
2023-07-12 16:33:40 +0100

    Include minimal template and styles.css for start

--- src/swag/cli.py
+++ src/swag/cli.py
@@ -1,5 +1,7 @@
 import os
 from pathlib import Path
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+import shutil
 
 import fire
 
@@ -18,8 +20,26 @@ def start(here=""):
     for subfolder in ["posts", "pages"]:
         if not os.path.exists(root / "content" / subfolder):
             os.mkdir(root / "content" / subfolder)
+
+    start_folder_path = Path(__file__).parents[1] / 'start'
+    for f in os.listdir(start_folder_path):
+        if '.html' in f:
+            shutil.copyfile(src=start_folder_path / f, dst=root /
+                            'templates' / f)
+        else:
+            shutil.copyfile(src=start_folder_path / f, dst=root /
+                            'assets' / f)
+
     swag.lorem_posts.main(root)
     swag.build.main(root)
+
+def serve():
+    class Handler(SimpleHTTPRequestHandler):
+        def __init__(self, *args, **kwargs):
+            super().__init__(directory='build', *args, **kwargs)
+    server = HTTPServer(("localhost", 8000), Handler)
+    server.serve_forever()
+
 
 
 def main():
